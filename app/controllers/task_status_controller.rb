c97ed3dd138728c824870078df2f079382e592b5
shu-one <231462690+shu-one@users.noreply.github.com>
2026-01-12 16:39:41 +0900

    別モデルに変更

--- app/controllers/task_status_controller.rb
+++ app/controllers/task_status_controller.rb
@@ -1,6 +1,6 @@
 class TaskStatusController < ApplicationController
   def index_api
-    task_status = Task::TaskStatus.all
+    task_status = TaskStatus.all
     render json: { task_status: task_status }
   end
 end
---
+++ app/models/task_status.rb
@@ -0,0 +1,15 @@
+class TaskStatus
+  include ActiveModel::Model
+
+  attr_accessor :status_code, :status_name
+
+  DATA = [
+    { status_code: "TODO", status_name: "未着手" },
+    { status_code: "DOING", status_name: "着手中" },
+    { status_code: "DONE", status_name: "完了" },
+  ].freeze
+
+  def self.all
+    DATA.map { |data| self.new(data) }
+  end
+end
